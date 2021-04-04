72dde4ab2e20845e7baab52010a4fb319f4d83cd
jeje <jerome.botiveau@epitech.eu>
2021-04-04 09:34:58 +0200

    spent-delete ok

--- apiDepense/routes/spent.routes.js
+++ apiDepense/routes/spent.routes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const spentController = require('../controllers/spent.controller')
 const spentCreateController = require('../controllers/spent-create.controller')
 const spentUpdateController = require('../controllers/spent-update.controller')
+const spentDeleteController = require('../controllers/spent-delete.controller')
 
 
 router.get('/spent', spentController.getSpent);
 router.post('/spent', spentCreateController.createSpent);
 router.put('/spent/:id', spentUpdateController.updateSpent);
+router.delete('/spent/:id', spentDeleteController.deleteSpent);
 /*router.get('/spents/:spentId', spentOneController.getSpent);*/
 
 
--- apiDepense/services/spent-delete.service.js
+++ apiDepense/services/spent-delete.service.js
@@ -2,21 +2,17 @@ const Spent = require("../models/spentModel");
 const { checkObjectId } = require('../helper/dbHelper');
 const constants = require('../constants');
 
-module.exports.updateSpent = async ({ id, updateInfo }) => {
+module.exports.deleteSpent = async ({ id }) => {
     try {
         checkObjectId(id);
-        let spent = await Spent.findOneAndUpdate(
-            { _id: id },
-            updateInfo,
-            { new: true }
-        )
+        let spent = await Spent.findByIdAndDelete(id);
         if (!spent) {
             throw new Error(constants.spentMessage.SPENT_NOT_FOUND);
         }
         return spent;
 
     } catch (error) {
-        console.log('Something went wrong: Service: updateSpent', error);
+        console.log('Something went wrong: Service: deleteSpent', error);
         throw new Error(error);
     }
 }
