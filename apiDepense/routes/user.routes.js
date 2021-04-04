380b946c7c2f25b735054ac2c7157d4ffe72290e
jeje <jerome.botiveau@epitech.eu>
2021-04-04 14:45:25 +0200

    user create ok

--- apiDepense/routes/user.routes.js
+++ apiDepense/routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const userController = require('../controllers/user.controller')
 //const userOneController = require('../controllers/user-getOne.controller')
-//const userCreateController = require('../controllers/user-create.controller')
+const userCreateController = require('../controllers/user-create.controller')
 //const userUpdateController = require('../controllers/user-update.controller')
 //const userDeleteController = require('../controllers/user-delete.controller')
 
@@ -11,7 +11,7 @@ const userController = require('../controllers/user.controller')
 //router.get('/signup', userController.getUser);
 router.get('/', userController.getUser);
 //router.get('/user/:id', userOneController.getSpentById);
-//router.post('/signup', userCreateController.createSpent);
+router.post('/signup', userCreateController.createUser);
 //router.put('/user/:id', userUpdateController.updateSpent);
 //router.delete('/user/:id', userDeleteController.deleteSpent);
 
