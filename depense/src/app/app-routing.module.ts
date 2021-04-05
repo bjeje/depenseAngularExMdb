f3610e646d4662a3b922c7b3dd2bd4fff1e4401c
jeje <jerome.botiveau@epitech.eu>
2021-04-05 10:39:23 +0200

    prepare-bound

--- depense/src/app/app-routing.module.ts
+++ depense/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {SignupPageComponent} from './pages/signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
