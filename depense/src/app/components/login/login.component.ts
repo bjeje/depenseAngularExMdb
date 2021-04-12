a543fa9314830e46d12bf211e5dadae520b743fd
jeje <jerome.botiveau@epitech.eu>
2021-04-12 09:11:25 +0200

    bind signup page to api

--- depense/src/app/components/login/login.component.ts
+++ depense/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '@app/services/user.services';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  login = '';
-  password = '';
+  constructor(private User: UserService, private router: Router) {}
+    loginForm = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl('')
+  });
 
-  constructor() {
+  async signin() {
+    (await this.User.signin(this.loginForm.value)).subscribe(
+      (data: any) => {
+        let token = data.body;
+        console.log(data);
+        localStorage.setItem('Token', token);
+        this.router.navigate([ '/home' ]);
+      },
+      /*(err: HttpErrorResponse) => {
+        console.log(err.error);
+        if (err.error.msg) {
+          this.snackBar.open(err.error.msg, 'Undo');
+        } else {
+          this.snackBar.open('Something Went Wrong!');
+        }
+      }*/
+    );
+    /*this.User.signin({login: login, password: password})*/
   }
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit() {}
 
 }
--- depense/src/app/services/user.services.ts
+++ depense/src/app/services/user.services.ts
@@ -8,12 +8,36 @@ import {AppComponent} from '../app.component';
 
 export class UserService {
 
-  baseUrl = '/';
+  baseUrl = '/user';
 
   constructor(private http: HttpClient) { }
 
   async getTests() {
     return this.http.get(AppComponent.url + this.baseUrl);
+  }
+
+  async getUser() {
+    return this.http.get(AppComponent.url + this.baseUrl);
+  }
+
+  async getUserById(id: number) {
+    return this.http.get(AppComponent.url + this.baseUrl + `${id}`);
+  }
+
+  async signup(user: object) {
+    return this.http.post(AppComponent.url + this.baseUrl + '/signup', user);
+  }
+
+  async signin(user: object) {
+    return this.http.post(AppComponent.url + this.baseUrl + '/signin', user);
+  }
+
+  async updateUser(id: number, user: object) {
+    return this.http.put(AppComponent.url + this.baseUrl + `${id}`, user);
+  }
+
+  async deleteUser(id: number) {
+    return this.http.delete(AppComponent.url + this.baseUrl + `${id}`);
   }
 
 }
