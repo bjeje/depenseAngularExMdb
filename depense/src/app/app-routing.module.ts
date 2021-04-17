7e700b17986f7d719c1d9b15006e4c92e6be8f5f
jeje <jerome.botiveau@epitech.eu>
2021-04-17 20:58:27 +0200

    authgard redirection

--- depense/src/app/app-routing.module.ts
+++ depense/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AppComponent} from './app.component';
 import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {SignupPageComponent} from './pages/signup-page/signup-page.component';
 import {HomePageComponent} from './pages/home-page/home-page.component';
+import { AuthGuard } from "./shared/auth.guard";
 
 const routes: Routes = [
   /*{ path: '', component: AppComponent },*/
   { path: '', component: LoginPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
-  { path: 'home', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent, canActivate: [AuthGuard]},
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
